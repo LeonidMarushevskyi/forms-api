867f645098841070bcbfc181a5800e64cc59c0ef
sorych <dmytro.sorych@osi.ca.gov>
2018-02-13 15:42:51 -0800

    coreApi version updated, FORMS_API_PORT parameter introduced

--- src/main/java/gov/ca/cwds/forms/web/rest/filters/RequestExecutionContextFilter.java
+++ src/main/java/gov/ca/cwds/forms/web/rest/filters/RequestExecutionContextFilter.java
@@ -2,7 +2,7 @@ package gov.ca.cwds.forms.web.rest.filters;
 
 import com.google.inject.Inject;
 import gov.ca.cwds.forms.web.rest.filters.RequestExecutionContext.Parameter;
-import gov.ca.cwds.security.shiro.web.PerryAuthenticatingFilter;
+import gov.ca.cwds.security.web.PerryAuthenticatingFilter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import javax.servlet.Filter;
